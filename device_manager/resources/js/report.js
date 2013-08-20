5d186dbba63d42509f613c2f115626a1d5fd1c16
Guilherme Martini <guilherme.martini20@gmail.com>
2013-08-19 23:15:10 -0300

    remocao dos filtros do relatorio e adicionado estilo das tabelas

--- device_manager/resources/js/report.js
+++ device_manager/resources/js/report.js
@@ -4,6 +4,18 @@ function linkTo(url, id){
 
 $(document).ready(
 function(){
+var acumulatedValeu = $("#acumulatedFilter").val();
+var filters = acumulatedValeu.split("&");
+console.log(filters);
+var html = "<li class='filter'><a class='filters' onClick='deleteFilter(this)' href='#'>{0}</a></li>";
+for (var i = 0; i < filters.length; i++){
+    console.log("Filtro", filters[i]);
+    console.log($('#choosen_filters'))
+    $("#choosen_filters").append(html.replace("{0}", filters[i]));
+}
+
+
+
 elements = document.getElementsByClassName('wide')
 for (var i=0;i< elements.length; i++){
     elements[i].parentNode.style.width = '100%';
@@ -23,16 +35,52 @@ $('#filterButton').click(function(){
             url = document.URL.split(replaceAll(acumulatedValeu, ' ', '%20'))[0]
             acumulatedValeu += "&";
         }
-        console.log(selectedOption);
-        console.log(textFilter);
         acumulatedValeu += selectedOption + "=" + textFilter;
-        console.log("url", url);
-        console.log("filtro", acumulatedValeu);
         window.location.href = url + acumulatedValeu + '/';
     }
 })
-
 })
+
+function deleteFilter(e){
+    var text = e.innerHTML;
+    var acumulated_filter = $('#acumulatedFilter').val();
+    var filters = acumulated_filter.split(text);
+    var filter = filters[0] + filters[1];
+    separated_filter = filter.split('&');
+    var new_filter = '';
+    for (var i = 0; i < separated_filter.length; i++){
+        if (i == 0 && (separated_filter[i])){
+            new_filter = separated_filter[i];
+        }
+        else{
+            if (separated_filter[i]){
+                console.log('novo_filtro', separated_filter[i]);
+                new_filter += '&' + separated_filter[i];
+            }
+        }
+    }
+    e.parentNode.removeChild(e);
+    $("#acumulatedFilter").val(new_filter);
+    var splited_urls = document.URL.split(text);
+    var splited_url = splited_urls[0] + splited_urls[1];
+    splited_url = splited_url.split('&');
+    var url = "";
+    for (var i = 0; i < splited_url.length; i++){
+        if (i == 0 && (splited_url[i])){
+            url = splited_url[i];
+        }
+        else{
+            console.log(splited_url[i])
+            if (splited_url[i] && splited_url.indexOf('=') != -1){
+                console.log('novo_filtro', splited_url[i]);
+                url += '&' + splited_url[i];
+            }
+        }
+    }
+    url += '/'
+    console.log('urlk', url);
+    window.location.href = url;
+};
 
 function replaceAll(string, token, newtoken) {
     while (string.indexOf(token) != -1) {
