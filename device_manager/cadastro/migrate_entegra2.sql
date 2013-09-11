061d1ea733ffc3238a87dbf8a5b95dadca1387b4
Guilherme Martini <guilherme.martini20@gmail.com>
2013-09-10 21:47:56 -0300

    criada permissao para usuario editar apenas o seu usuario

--- device_manager/cadastro/migrate_entegra2.sql
+++ device_manager/cadastro/migrate_entegra2.sql
@@ -2,3 +2,4 @@ alter TABLE cadastro_room ADD column syndic_id int;
 ALTER TABLE cadastro_room ADD CONSTRAINT cadastro_room_person_id_fkey FOREIGN KEY (syndic_id) REFERENCES cadastro_person(id);
 
 insert into cadastro_feature (name, description, uri) VALUES ('syndic_room', 'Listar, criar e editar salas que é sindico', 'room');
+insert into cadastro_feature (name, description, uri) VALUES ('edit_own_user', 'Editar o próprio usuário', 'user');
