b7f03f1469b4189c0ab9b3c418a99348007a2982
Guilherme Martini <guilherme.martini20@gmail.com>
2013-09-11 21:08:20 -0300

    adicionado decorator especifico para permissoes

--- device_manager/cadastro/decorator.py
+++ device_manager/cadastro/decorator.py
@@ -16,21 +16,17 @@ def my_login_required(function):
             user = User.objects.select_related().get(username=username)
             if not user:
                 return HttpResponseRedirect('/login/')
-            else:
-                path = request.path
-                if user.profile:
-                    for feature in user.profile.features.all():
-                        if path.find(feature.uri) != -1:
-                            return function(request, *args, **kw)
-                return HttpResponseRedirect('/forbidden/')
+        return function(request, *args, **kw)
     return wrapper
 
-#ef test_access_permission(function):
-#   def wrapper(request, *args, **kw):
-#       username=request.COOKIES.get("logged_user");
-#       user = User.objects.select_related().get(username=username)
-#       path = request.path
-#       for feature in user.profile.features:
-#           if path.find(feature.uri) != -1:
-#               return function(request, *args, **kw)
-#       return HttpResponseRedirect('/forbiden/')
+def test_access_permission(function):
+    def wrapper(request, *args, **kw):
+        username=request.COOKIES.get("logged_user");
+        user = User.objects.select_related().get(username=username)
+        path = request.path
+        if user.profile:
+            for feature in user.profile.features.all():
+                if path.find(feature.uri) != -1:
+                    return function(request, *args, **kw)
+        return HttpResponseRedirect('/forbidden/')
+    return wrapper
